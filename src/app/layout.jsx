dbe751e91e41022b142e73bc6fbddb9bf0db31b7
Ditzxxz <iyakitasma@gmail.com>
2026-02-16 12:35:23 +0900

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,6 @@
 import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
+import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/layout/Header'
 import Footer from '@/components/layout/Footer'
